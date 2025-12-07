48484472ff7a35141636785e30adccdb310e5758
3Flyx <129692521+3Flyx@users.noreply.github.com>
2025-12-07 22:31:08 +0100

    Fix

--- client/main.lua
+++ client/main.lua
@@ -36,55 +36,55 @@ end)
 
 function OpenMenu()
     local contextOptions = {}
-    lib.registerContext({
-        id = 'casino_menu',
-        title = locale('main_title'),
-        options = {
-            {
-                title = locale('buy_chips'),
-                description = locale('buy_chips_desc'),
-                icon = 'hand',
-                onSelect = function()
-                    local input = lib.inputDialog(locale('buy_title'), {
-                        {type = 'number', label = locale('buy_number_title'), description = locale('buy_number_desc'), required = true, min = 1, max = Config.ChipBuying.maxChips}
-                    })
-                    if not input[1] then return end
-                    if exports.ox_inventory:GetItemCount('money') < input[1] * Config.ChipPrice then 
-                        return lib.notify({
-                            title = locale('notify_title'),
-                            description = locale('not_enough_money'),
-                            type = 'error'
-                        })
-                        end
-                    TriggerServerEvent('flyx_casinoexchange/buyChips', {chipCount = input[1]})
+    contextOptions[#contextOptions+1] = {
+        title = locale('buy_chips'),
+        description = locale('buy_chips_desc'),
+        icon = 'hand',
+        onSelect = function()
+            local input = lib.inputDialog(locale('buy_title'), {
+                {type = 'number', label = locale('buy_number_title'), description = locale('buy_number_desc'), required = true, min = 1, max = Config.ChipBuying.maxChips}
+            })
+            if not input[1] then return end
+            if exports.ox_inventory:GetItemCount('money') < input[1] * Config.ChipPrice then 
+                return lib.notify({
+                    title = locale('notify_title'),
+                    description = locale('not_enough_money'),
+                    type = 'error'
+                })
                 end
-            },
-        }
-    })
+            TriggerServerEvent('flyx_casinoexchange/buyChips', {chipCount = input[1]})
+        end
+    }
 
     local chipCount = exports.ox_inventory:GetItemCount(Config.ChipItem)
     if chipCount > 0 then
         contextOptions[#contextOptions+1] = {
-            {
-                title = locale('sell_chips'),
-                description = locale('sell_chips_desc'),
-                icon = 'hand',
-                onSelect = function()
-                    local input = lib.inputDialog(locale('sell_title'), {
-                        {type = 'number', label = locale('sell_number_title'), description = locale('sell_number_desc'), required = true, min = 1, max = Config.ChipBuying.maxChips}
-                    })
-                    if not input[1] then return end
+            title = locale('sell_chips'),
+            description = locale('sell_chips_desc'),
+            icon = 'hand',
+            onSelect = function()
+                local input = lib.inputDialog(locale('sell_title'), {
+                    {type = 'number', label = locale('sell_number_title'), description = locale('sell_number_desc'), required = true, min = 1, max = Config.ChipBuying.maxChips}
+                })
+                if not input[1] then return end
 
-                    if input[1] > chipCount then 
-                        return lib.notify({
-                            title = locale('notify_title'),
-                            description = locale('not_enough_chips'),
-                            type = 'error'
-                        })
-                    end
-                    TriggerServerEvent('flyx_casinoexchange/sellChips', false, {chipCount = input[1]})
+                if input[1] > chipCount then 
+                    return lib.notify({
+                        title = locale('notify_title'),
+                        description = locale('not_enough_chips'),
+                        type = 'error'
+                    })
                 end
-            },
+                TriggerServerEvent('flyx_casinoexchange/sellChips', {chipCount = input[1]})
+            end
         }
     end
+
+    lib.registerContext({
+        id = 'casino_menu',
+        title = locale('main_title'),
+        options = contextOptions
+    })
+
+    lib.showContext('casino_menu')
 end
